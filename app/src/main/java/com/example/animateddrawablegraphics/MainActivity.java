90116b3c2bf5db7cb3d8b7e3abb63344d485a557
Dan Tra <dltra@fcps.edu>
2021-10-14 15:15:40 -0400

    Drawing Animated Vector: Morphing a path

--- app/src/main/java/com/example/animateddrawablegraphics/MainActivity.java
+++ app/src/main/java/com/example/animateddrawablegraphics/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.animateddrawablegraphics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    AppCompatImageView image;
+    AnimatedVectorDrawable animation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image = findViewById(R.id.image_animate);
+        animation = (AnimatedVectorDrawable) image.getDrawable();
+        animation.start();
     }
 }
